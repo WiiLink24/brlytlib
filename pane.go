f09a7e8001940e7c8ae5d40c36781a5b9b326cd8
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2022-07-22 19:10:19 -0400

    Fix pane

--- pane.go
+++ pane.go
@@ -38,19 +38,17 @@ func (r *Root) ParsePAN(data []byte) {
 	userData := strings.Replace(string(pane.UserData[:]), "\x00", "", -1)
 
 	xmlData := XMLPane{
-		Name:       name,
-		UserData:   userData,
-		Visible:    pane.Flag & 0x1,
-		Widescreen: (pane.Flag & 0x2) >> 1,
-		Flag:       pane.Flag,
-		Origin:     Coord2D{X: float32(pane.Origin % 3), Y: float32(pane.Origin / 3)},
-		Alpha:      pane.Alpha,
-		Padding:    0,
-		Translate:  Coord3D{X: pane.XTranslation, Y: pane.YTranslation, Z: pane.ZTranslation},
-		Rotate:     Coord3D{X: pane.XRotate, Y: pane.YRotate, Z: pane.ZRotate},
-		Scale:      Coord2D{X: pane.XScale, Y: pane.YScale},
-		Width:      pane.Width,
-		Height:     pane.Height,
+		Name:      name,
+		UserData:  userData,
+		Flag:      pane.Flag,
+		Origin:    Coord2D{X: float32(pane.Origin % 3), Y: float32(pane.Origin / 3)},
+		Alpha:     pane.Alpha,
+		Padding:   0,
+		Translate: Coord3D{X: pane.XTranslation, Y: pane.YTranslation, Z: pane.ZTranslation},
+		Rotate:    Coord3D{X: pane.XRotate, Y: pane.YRotate, Z: pane.ZRotate},
+		Scale:     Coord2D{X: pane.XScale, Y: pane.YScale},
+		Width:     pane.Width,
+		Height:    pane.Height,
 	}
 
 	r.Panes = append(r.Panes, Children{Pane: &xmlData})
@@ -69,18 +67,17 @@ func (r *Root) ParseBND(data []byte) {
 	userData := strings.Replace(string(pane.UserData[:]), "\x00", "", -1)
 
 	xmlData := XMLBND{
-		Name:       name,
-		UserData:   userData,
-		Visible:    pane.Flag & 0x1,
-		Widescreen: (pane.Flag & 0x2) >> 1,
-		Origin:     Coord2D{X: float32(pane.Origin % 3), Y: float32(pane.Origin / 3)},
-		Alpha:      pane.Alpha,
-		Padding:    0,
-		Translate:  Coord3D{X: pane.XTranslation, Y: pane.YTranslation, Z: pane.ZTranslation},
-		Rotate:     Coord3D{X: pane.XRotate, Y: pane.YRotate, Z: pane.ZRotate},
-		Scale:      Coord2D{X: pane.XScale, Y: pane.YScale},
-		Width:      pane.Width,
-		Height:     pane.Height,
+		Name:      name,
+		UserData:  userData,
+		Flag:      pane.Flag,
+		Origin:    Coord2D{X: float32(pane.Origin % 3), Y: float32(pane.Origin / 3)},
+		Alpha:     pane.Alpha,
+		Padding:   0,
+		Translate: Coord3D{X: pane.XTranslation, Y: pane.YTranslation, Z: pane.ZTranslation},
+		Rotate:    Coord3D{X: pane.XRotate, Y: pane.YRotate, Z: pane.ZRotate},
+		Scale:     Coord2D{X: pane.XScale, Y: pane.YScale},
+		Width:     pane.Width,
+		Height:    pane.Height,
 	}
 
 	r.Panes = append(r.Panes, Children{BND: &xmlData})
